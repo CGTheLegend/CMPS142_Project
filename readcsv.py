13f8ce7dc12d3dafb75861ee29d31c8c24516426
Cristian Gonzalez <cgtheslug@gmail.com>
2017-03-21 16:42:48 -0700

    Updated readcsv - Returns dataset, features and targets - dataset still not tensor - Generates target values (really slow, might be a better way to do this)

--- readcsv.py
+++ readcsv.py
@@ -4,10 +4,23 @@ import pandas as pd
 
 
 def readcsv(filename):
-    # Read in CSV with pandas
+    # read in CSV with pandas
     ds = pd.read_csv(filename, sep=',', header=None)
     # convert to tensor
     dataset = tf.contrib.learn.extract_pandas_matrix(ds)
     features = tf.convert_to_tensor(dataset[0,:], dtype=tf.string)
     gross = tf.convert_to_tensor(dataset[1:,8], dtype=tf.float32)
-    return dataset, features, gross
+    budget = tf.convert_to_tensor(dataset[1:,22], dtype=tf.float32)
+    targets = tf.Variable(tf.zeros(5044), dtype=tf.float32)
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        # generate targets values (gross>budget)
+        for i in range(5043):
+            if sess.run(gross[i]) > sess.run(budget[i]):
+                sess.run(targets[i].assign(1))
+
+    return dataset, features, targets
