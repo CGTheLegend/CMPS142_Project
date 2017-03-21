700b552e6987f8413f2058ce699f1da3a28d1752
Cristian Gonzalez <cgtheslug@gmail.com>
2017-03-20 20:00:16 -0700

    Added comments

--- readdataset.py
+++ readdataset.py
@@ -3,13 +3,14 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
+# ignore warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 # Read in CSV with pandas
 ds = pd.read_csv('movie_metadata.csv', sep=',', header=None)
 print(ds)
 
-# convert to tensor 
+# convert to tensor
 dataset = tf.contrib.learn.extract_pandas_matrix(ds)
 sess = tf.Session()
 print(dataset)
