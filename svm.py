f1ae23cb90bf44e56932195073fc8815318690a5
Cristian Gonzalez <cgtheslug@gmail.com>
2017-03-24 21:08:16 -0700

    Added encoder & Test Data - encoded features for model - generate a test data set - run test data set

--- svm.py
+++ svm.py
@@ -4,9 +4,11 @@ import tensorflow as tf
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import train_test_split
 
 
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+np.set_printoptions(threshold='nan')
 
 class MultiColumnLabelEncoder:
     def __init__(self,columns = None):
@@ -16,11 +18,6 @@ class MultiColumnLabelEncoder:
         return self
 
     def transform(self,X):
-        '''
-        Transforms columns of X specified in self.columns using
-        LabelEncoder(). If no columns specified, transforms all
-        columns in X.
-        '''
         output = X.copy()
         if self.columns is not None:
             for col in self.columns:
@@ -48,25 +45,34 @@ targets = np.array
 targets = np.greater(gross, budget)
 _NaNs = pd.isnull(targets)
 targets[_NaNs] = False
-# remove unquantifiable colums (i.e. names)
+# remove unquantifiable colums (i.e. names, iMDb link)
 dataset_ = np.array
-dataset_ = dataset_raw[1:,[2,3,4,5,7,8,12,13,15,18,22,23,24,25,26,27]]
+dataset_ = dataset_raw[1:,[2,3,4,5,7,8,9,12,13,15,18,19,20,21,22,23,24,25,26,27]]
+#                          0 1 2 3 4 5 6  7  8  9 10 11 12 13 14 15 16 17 18 19
 _NaNs = pd.isnull(dataset_)
 dataset_[_NaNs] = 0
 # encode lables
-dataset = MultiColumnLabelEncoder(columns = [9,19,20,2]).fit_transform(dataset_)
-
+dataset = dataset_
+le_genre = LabelEncoder()
+dataset[:, 6] = le_genre.fit_transform(dataset_[:,6])
+le_language = LabelEncoder()
+dataset[:, 11] = le_language.fit_transform(dataset_[:,11])
+le_country = LabelEncoder()
+dataset[:, 12] = le_country.fit_transform(dataset_[:,12])
+le_content_rating = LabelEncoder()
+dataset[:, 13] = le_content_rating.fit_transform(dataset_[:,13])
+# run SVC
 clf = SVC()
 clf.fit(dataset, targets)
-
 SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+    decision_function_shape=None, degree=3, gamma='auto', kernel='sigmoid',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
 
-# read in test dataset
-test = dataset[4500:,:]
+# generate test dataset
+X_train, X_test, y_train, y_test = train_test_split(dataset, targets, test_size=0.3, random_state=0.5)
 
-predictions = clf.predict(dataset)
-error = np.equal(targets[4500:], predictions)
-print(np.sum(error))
+predictions = clf.predict(X_test)
+error = np.equal(y_test, predictions)
+error_percent = np.sum(error)/1513 * 100
+print  error_percent, "%"
